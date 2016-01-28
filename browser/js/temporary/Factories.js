24bf1e02da6e51a5b6839f3e2e5bd18a7068c716
ajpz <perez.jonthan@gmail.com>
2016-01-28 17:53:32 -0500

    Updated game plan and new folders for directives and factories

--- browser/js/temporary/Factories.js
+++ browser/js/temporary/Factories.js
@@ -5,61 +5,66 @@ FirebaseFactory
  - returns firebase DB reference
 
 GameFactory:
-  - initializes gameState FireBase
-  - intiailizes gameState view
-  - deals Roles
-  - deals Infections
-  - initiates game:
-    - activates Action Phase when the TURN matches USER
+	- initializes gameState FireBase
+	- intiailizes gameState view
+	- deals Roles
+	- deals Infections
+	- initiates game:
+		- activates Action Phase when the TURN matches USER
 
-ActionFactory:
-  calculatePossibleActions()
+utilsService:
+	- decorator GameSet wrapper function
 
-  methods:
-   - playTurn()
-   - actionPhase()
-   - drawPhase()
-   - discardPhase()
-   - infectionPhase()
-   - epidemic()
-   - eventInterrupt()
+ActionFactory: //MAYBE BREAK THIS INTO SUB-FACTORIES - PHASEFACTORY?
+	calculatePossibleActions()
 
-CardFactory:
-  methods:
-    - createPlayerDeck()
-    - createInfectionDeck()
-    - shuffleDeck()
-    - pickCardFromTop()
-    - pickCardFromBottom()
-    - shuffleDiscardDeck()
-    - addCardsToTop()
-    - isEmpty()
-    - etc...
+	methods:
+	 - playTurn()
+	 - actionPhase()
+	 - drawPhase()
+	 - discardPhase()
+	 - infectionPhase()
+	 - epidemic()
+	 - eventInterrupt()
+
+CardFactory: (takes Deck )
+	methods:
+		- createPlayerDeck()
+		- createInfectionDeck()
+		- shuffleDeck()
+		- pickCardFromTop()
+		- pickCardFromBottom()
+		- shuffleDiscardDeck()
+		- addCardsToTop()
+		- isEmpty()
+		- etc...
 
 InfectionFactory:
-  infectionRateArray = [2, 2,, 3, 3, 4, 4];
-  methods:
-    - infectCity(number) // max 3 then outbreak
-    - infectConnections() //for outbreak
-    - initializeInfections()
-    - isEmpty(color)
+	infectionRateArray = [2, 2,, 3, 3, 4, 4];
+	methods:
+		- infectCity(number) // max 3 then outbreak
+		- infectConnections() //for outbreak
+		- initializeInfections()
+		- isEmpty(color)
+	TODO capture previous infections during outbreaks (recursive closure)
 
 RenderFactory:
-  methods:
-    - renderAction()
-    - renderShuffle()
-    -
+	methods:
+		- renderAction()
+		- renderShuffle()
+		-
 
 
 EventCardFactory:
 
 CityCardDirective:
-  - selectable for discarding/spending/trading purposes
+	- selectable for discarding/spending/trading purposes
 
 EventCardDirective:
-  - listens on USER click event
-  - event emitter - updates eventCardInEffect game state
+	- listens on USER click event
+	- event emitter - updates eventCardInEffect game state
 
+ActionSelectorDirective:
 
 
 Map has to have user interactions
