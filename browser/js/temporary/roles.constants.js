838ef248e92c9dc637fb3059bac91ef7db2df9e8
Victor Hom <vh62@cornell.edu>
2016-01-28 21:51:12 -0500

    made messups to moving the temporary folder

---
+++ browser/js/temporary/roles.constants.js
@@ -0,0 +1,18 @@
+const roles = {
+  medic: {
+    ability: 'The medic removes all cubes of the same color when treating a disease. If a disease has been cured, they automatically remove all disease cubes of that color from a city simply by entering it or being there. This does not take an action.',
+    icon: "<URL_TO_ICON>"
+  },
+  scientist: {
+    ability: 'The scientist needs only four (not five) city cards of the same disease color to discover a cure for that disease.',
+    icon: "<URL_TO_ICON>"
+  },
+  operationsExpert: {
+    ability: 'The operations expert may, as an action, either build a research station in their city without discarding a card or, once per turn, move from a research station to any city by discarding any city card.',
+    icon: "<URL_TO_ICON>"
+  },
+  researcher: {
+    ability: 'The researcher may give any city card from their hand to another player in the same city without this card having to match the researcher\'s city. The transfer must be from the researcher\'s hand to the other player\'s hand, but it can occur on either player\'s turn',
+    icon: "<URL_TO_ICON>"
+  }
+};
