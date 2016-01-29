990b74855621e205624bb654d12edd2949a70abd
opperator <julie.e.oppermann@gmail.com>
2016-01-29 11:39:14 -0500

    removed temporary folder, moved constants into 'Factories' folder and set them up so they can be injected into factories when needed

---
+++ browser/js/factories/ob.initialization.constants.js
@@ -0,0 +1,21 @@
+// these are the static conditions for a new game.
+
+app.constants('Initialize', {
+  gameOver: false,
+  //map: <create an empty map>
+  researchCenters: ['Atlanta'],
+  outbreakLevel: 0,
+  infectionLevelIndex: 0,
+  isCured: {
+    red: false,
+    blue: false,
+    yellow: false,
+    black: false
+  },
+  isEradicated: {
+    red: false,
+    blue: false,
+    yellow: false,
+    black: false
+  }
+});
