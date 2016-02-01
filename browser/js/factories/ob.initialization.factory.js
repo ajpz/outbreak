d1e9eaf8fae09cb4e40512d89ef9f06a8e924462
ajpz <perez.jonthan@gmail.com>
2016-02-01 17:42:39 -0500

    add intializationFactory and related tests

--- browser/js/factories/ob.initialization.factory.js
+++ browser/js/factories/ob.initialization.factory.js
@@ -1,11 +1,13 @@
-app.factory('InitializationFactory', function($rootScope, CitiesCardFactory, InfectionFactory, CardFactory, SetToGameState) {
+app.factory('InitFactory', function(CitiesCardFactory, InfectionFactory, CardFactory) {
 
   //TODO: GameFactory to remove all references to CitiesCardFactory and InfectionFactory
 
+  const cardNumMap = { "2": 4, "3": 3, "4": 2 };
+  const numEpidemicMap = { Introductory: 4, Standard: 5, Heroic: 6 };
+
   const createPlayerDeck = CitiesCardFactory.createPlayerDeck;
   const createInfectionDeck = InfectionFactory.createInfectionDeck;
-  const dealPlayerCards = function(workingState) {
-    const cardNumMap = { "2": 4, "3": 3, "4": 2 };
+  const dealCardsToGamers = function(workingState) {
     let gamers = workingState.gamers;
     let numCards = cardNumMap[workingState.gamers.length.toString()];
 
@@ -23,47 +25,56 @@ app.factory('InitializationFactory', function($rootScope, CitiesCardFactory, Inf
 
 
   return {
+    //TODO: to create time-delay for UI visualization, create promisified
+    // version of SetToGameState that:
+    //  1) invokes function
+    //  2) SetToGameState()
+    //  3) setTimeout delay before resolving
+    //  4) subsequent methods are invoked once the above is finished
 
 
     initializeGameElements: function(workingState) {
-      SetToGameState(createPlayerDeck(workingState));  // returns deck
-      SetToGameState(createInfectionDeck(workingState)); // returns deck
-      SetToGameState(dealPlayerCards(workingState)); //
-
-
+      // defaults right now to 'Introductory' difficulty
+      workingState.playerDeck = createPlayerDeck();
+      console.log('length is ', workingState.playerDeck.length);
+      workingState.infectionDeck = createInfectionDeck();
+      return dealCardsToGamers(workingState);
     },
     giveUserARole: function() {
-
+      //write this when we implement multiple players and a true lobby
+      //should have role-array with all 7 characters
+      //should randomly pop-off roles and send them to GameFactory
+      //GameFactory will assigned these roles, to users as they join the game
     }
   }
 });
 
-  $rootScope.$on('statusChanged', function(event, payload) {
+  // $rootScope.$on('statusChanged', function(event, payload) {
 
-    switch(localState.status) {
+  //   switch(localState.status) {
 
-      case 'initialization' :
-        localState = InitializationFactory.initializeGameElements(_.cloneDeep(localState));
-        localState.$save();
-        break;
+  //     case 'initialization' :
+  //       localState = InitializationFactory.initializeGameElements(_.cloneDeep(localState));
+  //       localState.$save();
+  //       break;
 
-      case 'inProgress' :
+  //     case 'inProgress' :
 
-        break;
+  //       break;
 
-      case 'gameOver' ;
+  //     case 'gameOver' :
 
-        break;
-    }
-  });
+  //       break;
+  //   }
+  // });
 
 
-  var saveToFirebase = function(workingState) {
+  // var saveToFirebase = function(workingState) {
 
-    localState = workingState;
-    return localState.$save()
-      .then(function(localState){
-        return localState;
-      })
+  //   localState = workingState;
+  //   return localState.$save()
+  //     .then(function(localState){
+  //       return localState;
+  //     })
 
-  };
+  // };
--- browser/js/factories/ob.setToGameState.factory.js
+++ browser/js/factories/ob.setToGameState.factory.js
@@ -1,19 +1,3 @@
-// app.factory('SetToGameState', function(GameFactory) {
-//   return function(func) {
-//     return function(...args) {
-//       var updatedState = func(...args);
-//       // seems hard to test if this method also calls the setState
-//       // from what I see here, it's assuming that the updatedState
-//       // is a either modifying the GameFactory.gameState obj or it returns an object for updating
-//       GameFactory.saveToFirebase(updatedState);
-//       return updatedState;
-//     }
-//   }
-// });
-
-
-
-
 app.factory('SetToGameState', function(GameFactory) {
   return function(func) {
     return function(...args) {
@@ -21,7 +5,23 @@ app.factory('SetToGameState', function(GameFactory) {
       // seems hard to test if this method also calls the setState
       // from what I see here, it's assuming that the updatedState
       // is a either modifying the GameFactory.gameState obj or it returns an object for updating
-      return GameFactory.saveToFirebase(updatedState);
+      // GameFactory.saveToFirebase(updatedState);
+      return updatedState;
     }
   }
 });
+
+
+
+
+// app.factory('SetToGameState', function(GameFactory) {
+//   return function(func) {
+//     return function(...args) {
+//       var updatedState = func(...args);
+//       // seems hard to test if this method also calls the setState
+//       // from what I see here, it's assuming that the updatedState
+//       // is a either modifying the GameFactory.gameState obj or it returns an object for updating
+//       return GameFactory.saveToFirebase(updatedState);
+//     }
+//   }
+// });
