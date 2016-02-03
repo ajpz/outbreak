d8b0cec606d39de813914935f506f542e68908ad
opperator <julie.e.oppermann@gmail.com>
2016-02-02 21:49:10 -0500

    added directives for outbreak level, infection rate, cures, and still available... made a bunch of icons for the game to look AMAZEBALLS

--- browser/js/directives/infection-rate/ob.infection-rate.js
+++ browser/js/directives/infection-rate/ob.infection-rate.js
@@ -3,16 +3,26 @@ app.directive("infectionRate", function($rootScope){
 		restrict: 'E',
 		templateUrl: 'js/directives/infection-rate/ob.infection-rate.html',
 		scope: {},
-		link: function(scope){
+		link: function(scope, element){
 			scope.infectionRateIndex = 0;
+      var payload = {
+        infectionRateIndex: 3
+      }
 
-			scope.changeTheIndex = function(payload){
-				scope.infectionRateIndex = payload.infectionRateIndex;
-				$("#radio_"+scope.infectionRateIndex).prop("checked", true)
+      scope.changeTheIndex = function(payload){
+        scope.infectionRateIndex = payload.infectionRateIndex;
+        var elem = $(element).find("#index_"+scope.infectionRateIndex);
+        for(var i=0; i<7; i++){
+          if(i!==scope.infectionRateIndex){
+            var elem = $(element).find("#index_"+i)
+            elem.children("img").attr("src", "http://i.imgur.com/5I0tivz.png")
+          }
+        }
+      }
 
-			}
+      scope.changeTheIndex(payload)
 
-			$rootScope.$on('stateChange', function(event, payload){
+      $rootScope.$on('stateChange', function(event, payload){
 				if(scope.infectionRateIndex !== payload.infectionRateIndex){
 					changeTheIndex(payload);
 				}
@@ -21,3 +31,7 @@ app.directive("infectionRate", function($rootScope){
 		}
 	};
 });
+
+
+//jQuery( "a:contains(OpenStreetMap)" ).remove()
+//jQuery( "a:contains(Improve this map)" ).remove()
--- browser/js/directives/outbreak-level-directive/ob.outbreak-level.js
+++ browser/js/directives/outbreak-level-directive/ob.outbreak-level.js
@@ -3,16 +3,26 @@ app.directive("outbreakLevel", function($rootScope){
 		restrict: 'E',
 		templateUrl: 'js/directives/outbreak-level-directive/ob.outbreak-level.html',
 		scope: {},
-		link: function(scope){
+		link: function(scope, element){
 			scope.outbreakLevel = 0;
 			//the database should initialize the outbreak level so the above line should not be necessary
-
+      var payload = {
+        outbreakLevel: 4
+      }
 			scope.changeTheLevel = function(payload){
 				scope.outbreakLevel = payload.outbreakLevel;
-				$("#radio_"+scope.outbreakLevel).prop("checked", true)
-
+        var elem = $(element).find("#level_"+scope.outbreakLevel);
+        for(var i=1; i<8; i++){
+          if(i!==scope.outbreakLevel){
+            var elem = $(element).find("#level_"+i)
+            elem.children("img").attr("src", 'http://i.imgur.com/GLfit2U.png')
+          }
+        }
 			}
-			$rootScope.$on('stateChange', function(event, payload){
+
+      scope.changeTheLevel(payload)
+
+      $rootScope.$on('stateChange', function(event, payload){
 				if(scope.outbreakLevel !== payload.outbreakLevel){
 					changeTheLevel(payload);
 				}
