bafa731f0e3502df10dbc285ca70bf412746a96e
ajpz <perez.jonthan@gmail.com>
2016-02-03 16:09:26 -0500

    switched firebase urls

--- browser/js/factories/ob.game.factory.js
+++ browser/js/factories/ob.game.factory.js
@@ -10,7 +10,7 @@ app.factory('GameFactory', function(Firebase, Cities, $firebaseObject, $rootScop
    // homburger: 'https://radiant-fire-7882.firebaseio.com/outbreak'
    // ajpz:      'https://outbreaktest.firebaseio.com/outbreak'
    // dthorne: 'https://outbreak-daniel.firebaseio.com/'
-  const ref = new Firebase('https://outbreak-daniel.firebaseio.com/');
+  const ref = new Firebase('https://outbreaktest.firebaseio.com/outbreak');
   let outbreak  = $firebaseObject(ref);
   FlowFactory();
 
