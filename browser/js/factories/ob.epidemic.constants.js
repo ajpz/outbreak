28749850dedb681ed650c34527b3db56686fc732
opperator <julie.e.oppermann@gmail.com>
2016-02-05 16:31:01 -0500

    added images for the event cards and epidemic card

--- browser/js/factories/ob.epidemic.constants.js
+++ browser/js/factories/ob.epidemic.constants.js
@@ -5,5 +5,6 @@ app.constant('Epidemic', {
     '(1) Increase: Move the infection rate marker forward one space',
     '(2) Infect: Draw the bottom card from the infection deck and put three cubes on the city. Discard that card.',
     '(3) Intensify: Shuffle the cards in the infection discard pile and put them on top of the infection deck.'
-    ]
+    ],
+  cardFront: 'http://i.imgur.com/iMudkxe.png'
 });
--- browser/js/factories/ob.events.constants.js
+++ browser/js/factories/ob.events.constants.js
@@ -6,7 +6,8 @@ app.constant('Events', {
     key: 'airlift',
     name: 'Airlift',
     color: 'green',
-    text: 'Move any one pawn to any city. Get permission before moving another players pawn.'
+    text: 'Move any one pawn to any city. Get permission before moving another players pawn.',
+    cardFront: 'http://i.imgur.com/Ua0o7Md.png'
   },
   oneQuietNight: {
     verb: 'Mitigate',
@@ -14,7 +15,8 @@ app.constant('Events', {
     key: 'oneQuietNight',
     name: 'One Quiet Night',
     color: 'green',
-    text: 'Skip the next infect city step (do not flip over any infection cards).'
+    text: 'Skip the next infect city step (do not flip over any infection cards).',
+    cardFront: 'http://i.imgur.com/PofSDHG.png'
   },
   governmentGrant: {
     verb: 'Build',
@@ -22,7 +24,8 @@ app.constant('Events', {
     key: 'governmentGrant',
     name: 'Government Grant',
     color: 'green',
-    text: 'Add one research station to any city (no discard needed).'
+    text: 'Add one research station to any city (no discard needed).',
+    cardFront: 'http://i.imgur.com/4Vg03j7.png'
   },
   forecast: {
     verb: 'Mitigate',
@@ -30,7 +33,8 @@ app.constant('Events', {
     key: 'forecast',
     name: 'Forecast',
     color: 'green',
-    text: 'Draw, look at, and rearrange the top six cards of the infection deck and put them back on top.'
+    text: 'Draw, look at, and rearrange the top six cards of the infection deck and put them back on top.',
+    cardFront: 'http://i.imgur.com/8HYBqwK.png'
   },
   resilientPopulation: {
     verb: 'Mitigate',
@@ -38,7 +42,8 @@ app.constant('Events', {
     key: 'resilientPopulation',
     name: 'Resilient Population',
     color: 'green',
-    text: 'Remove any one card in the infection discard pile from the game. You may play this between the infect and intensify steps of an epidemic.'
+    text: 'Remove any one card in the infection discard pile from the game. You may play this between the infect and intensify steps of an epidemic.',
+    cardFront: 'http://i.imgur.com/lApYPeL.png'
   }
 
 });
