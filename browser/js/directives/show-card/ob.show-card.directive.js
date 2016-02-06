c4c90e5c67110b830bf51eba22c63447e35cec3c
opperator <julie.e.oppermann@gmail.com>
2016-02-06 11:48:06 -0500

    show-card directive is showing player card images during draw

--- browser/js/directives/show-card/ob.show-card.directive.js
+++ browser/js/directives/show-card/ob.show-card.directive.js
@@ -5,8 +5,6 @@ app.directive('showCard', function($rootScope) {
     scope: {},
     link: function(scope, element, attr) {
 
-      console.log("\n\n\n\n\nDIRECTIVE IS REGISTERING");
-
       scope.isCurrentlyDrawPhase = false;
       scope.cardImages = [];
 
@@ -15,17 +13,11 @@ app.directive('showCard', function($rootScope) {
         if(payload.gameState.currentPhase === 'draw') {
           scope.isCurrentlyDrawPhase = true;
         }
-        console.log('heard stateChange', scope.cardImages, scope.isCurrentlyDrawPhase);
 
-        console.log('\n\nCURRENT PHASE IS ', payload.gameState.currentPhase,'\n')
 
-        // if(!scope.isCurrentlyDrawPhase) {
-        //   scope.cardImages = [];
-        // }
       });
 
       $rootScope.$on('drawCard', function(event, payload) {
-        console.log('heard drawCard ', payload.cardImage)
         scope.cardImages.push(payload.cardImage);
       });
 
