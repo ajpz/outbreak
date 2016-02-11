4ed603e436e64d16840d49907eb381113e15e6a9
opperator <julie.e.oppermann@gmail.com>
2016-02-11 18:46:05 -0500

    fixed accordion to be able to open multiple hands

--- browser/js/directives/gameover/ob.gameover.js
+++ browser/js/directives/gameover/ob.gameover.js
@@ -1,16 +1,17 @@
-app.directive("gameOver", function($rootScope) {
+app.directive("gameOver", function($rootScope, Reasons) {
   return {
     restrict: 'E',
     templateUrl: 'js/directives/gameover/ob.gameover.html',
     scope: {},
     link: function(scope, element) {
-
-      scope.win = gameState.gameOver.win
-      scope.lose = gameState.gameOver.lose
+      scope.win = false;
+      scope.lose = false;
 
       $rootScope.$on('stateChange', function(event, payload) {
         let gameState = payload.gameState;
-        scope.changeTheIndex(gameState);
+        scope.win = (gameState.status === 'gameOver' && gameState.gameOver.win)
+        scope.lose = (gameState.status === 'gameOver' && !gameState.gameOver.win)
+        scope.reason = Reasons[gameState.gameOver.lossType];
 
       })
 
