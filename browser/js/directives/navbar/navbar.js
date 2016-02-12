c28f6c25e0f85c523495b7135751437f279bac16
Victor Hom <vh62@cornell.edu>
2016-02-12 10:43:00 -0500

    fixed the research center effect

--- browser/js/directives/navbar/navbar.js
+++ browser/js/directives/navbar/navbar.js
@@ -79,7 +79,8 @@ app.directive('navbar', function ($rootScope, AuthService, AUTH_EVENTS, $state, 
             $rootScope.$broadcast('discardCardChosen', card);
           }
 
-        } else if (localCopyOfState.currentPhase === "actions" && card.type === "eventCard"
+        }
+        else if (localCopyOfState.currentPhase === "actions" && card.type === "eventCard"
             && (localStorage.getItem('user') === localCopyOfState.gamers[localCopyOfState.gamerTurn].username)) {
           // making use of the scope.eventAction ability since it should apply similarly
           // if the current player has an event card which we did not take care of.
@@ -103,6 +104,7 @@ app.directive('navbar', function ($rootScope, AuthService, AUTH_EVENTS, $state, 
       scope.eventAction = function(card) {
         // added in a quick check to make sure that the user is also the same user that is going
         // this would ensure that all  other players don't get the ability to get that ability
+        //
         if (localCopyOfState.currentPhase === "actions" && card.type === "eventCard"
           && (localStorage.getItem('user') === localCopyOfState.gamers[localCopyOfState.gamerTurn].username)) {
           if (card.key === "airlift"){
@@ -110,6 +112,7 @@ app.directive('navbar', function ($rootScope, AuthService, AUTH_EVENTS, $state, 
           } else if (card.key === "oneQuietNight") {
             playOneQuietNight();
           } else if (card.key === "governmentGrant") {
+            console.log("first in event action arae");
             scope.eventCardOptions.showGovernmentGrant = true;
             scope.notifyGovernmentGrantChange();
           } else if (card.key === "forecast"){
@@ -182,10 +185,15 @@ app.directive('navbar', function ($rootScope, AuthService, AUTH_EVENTS, $state, 
       }
 
       scope.notifyGovernmentGrantChange = function() {
+        console.log("in notify government grant area ", scope.governmentGrant.city);
         // generate a list of cities that do not have research centers as options to build a research center
         // researchCenterLocations is an array of locations city keys
+
         scope.governmentGrantCities = localCopyOfState.cities.filter(function(city) {
-          return (localCopyOfState.researchCenterLocations.indexOf(city.key) === -1);
+          if (localCopyOfState.researchCenterLocations.indexOf(city.key) === -1){
+            console.log(city);
+            return true;
+          }
         })
       }
 
