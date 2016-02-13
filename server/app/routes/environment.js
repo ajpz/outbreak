a1904ed5cd48dacdf57c3b03f712b15994384c20
Leon Thorne <ldthorne@brandeis.edu>
2016-02-13 16:34:03 -0500

    pls heroku stop

--- server/app/routes/environment.js
+++ server/app/routes/environment.js
@@ -1,6 +1,7 @@
 var app = require('express').Router();
 
 app.get('/', function(req,res,next){
+	console.log(process.env)
 	if(process.env.NPM_CONFIG_PRODUCTION){
 		res.status(201).send(true)
 	}else{
