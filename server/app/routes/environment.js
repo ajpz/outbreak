24ff0c59325bf0ff0ea4ce4f9c9d67eec42b6e54
Leon Thorne <ldthorne@brandeis.edu>
2016-02-13 16:36:23 -0500

    i have faith... kinda

--- server/app/routes/environment.js
+++ server/app/routes/environment.js
@@ -2,7 +2,8 @@ var app = require('express').Router();
 
 app.get('/', function(req,res,next){
 	console.log(process.env)
-	if(process.env.NPM_CONFIG_PRODUCTION){
+	console.log(process.env.NODE_ENV)
+	if(process.env.NODE_ENV && process.env.NODE_ENV==="production"){
 		res.status(201).send(true)
 	}else{
 		res.status(201).send(false)
