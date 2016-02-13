7489563fec44c1fbe0f2387ba302170c3706130d
opperator <julie.e.oppermann@gmail.com>
2016-02-13 13:45:54 -0500

    added in help icon with link to game rules, and fixed game over loss page

---
+++ browser/js/directives/game-rules/ob.gamerules.js
@@ -0,0 +1,10 @@
+app.directive("gamerules", function($rootScope) {
+  return {
+    restrict: 'E',
+    templateUrl: 'js/directives/game-rules/ob.game-rules.html',
+    scope: {},
+    link: function(scope) {
+
+    }
+  };
+});
